1795fce28b6c76afc5ec79634918942390226bc2
Neo <alghoochi@gmail.com>
2024-12-05 15:29:07 +0100

    load env variables as config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/alireza-msv/jet/internal/config"
+)
 
 func main() {
-	fmt.Println("EHRE")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+const defaultSchedule = "0 0 * * *"
+
+type Config struct {
+	AccountID    string
+	ClientID     string
+	ClientSecret string
+	Subdomain    string
+	Schedule     string
+}
+
+func LoadConfig() (*Config, error) {
+	accountID, err := readEnvVariable("ACCOUNT_ID", true)
+	if err != nil {
+		return nil, err
+	}
+
+	clientID, err := readEnvVariable("CLIENT_ID", true)
+	if err != nil {
+		return nil, err
+	}
+
+	clientSecret, err := readEnvVariable("CLIENT_SECRET", true)
+	if err != nil {
+		return nil, err
+	}
+
+	subdomain, err := readEnvVariable("SUBDOMAIN", true)
+	if err != nil {
+		return nil, err
+	}
+
+	schedule, err := readEnvVariable("SCHEDULE", false)
+	if schedule != "" {
+		schedule = defaultSchedule
+	}
+
+	return &Config{
+		AccountID:    accountID,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Subdomain:    subdomain,
+		Schedule:     schedule,
+	}, nil
+}
+
+func readEnvVariable(name string, required bool) (string, error) {
+	val := os.Getenv(name)
+	if required && val == "" {
+		return "", fmt.Errorf("Env variable not present: %s", name)
+	}
+
+	return val, nil
+}
